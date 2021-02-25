22092c098eecc99ffaaa83f6a81785bd82e54f03
sori <sori.coralcat@gmail.com>
2021-02-25 10:44:38 +0900

    탭메뉴 기능 추가

--- src/assets/script.js
+++ src/assets/script.js
@@ -31,6 +31,38 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 
 
+
+    // Tab Menu
+    const tabMenus = document.querySelectorAll('.tab-menu li')
+    const tabContents = document.querySelectorAll('.tab-content section')
+
+
+    const activeSection = (e) => {
+        e.stopPropagation();
+        let menuIndex = [...tabMenus].indexOf(e.target);
+
+        tabMenus.forEach(menu => {
+            [...tabMenus].indexOf(menu) === menuIndex
+                ? menu.classList.add('active')
+                : menu.classList.remove('active')
+        });
+
+        tabContents.forEach(content => {
+            [...tabContents].indexOf(content) === menuIndex
+                ? content.classList.add('active')
+                : content.classList.remove('active')
+        });
+    };
+
+    [...tabMenus][0].classList.add('active');
+    [...tabContents][0].classList.add('active');
+
+    tabMenus.forEach(menu => {
+        menu.addEventListener('click', activeSection);
+    })
+
+
+
     // Heart
     const icoHearts = document.querySelectorAll('.heart')
     icoHearts.forEach(heart => {
